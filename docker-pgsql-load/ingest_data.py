bcfeae0a68f97dfad7c3540817fd9d3a6946d19e
atosaleti <ravialeti00@gmail.com>
2025-05-05 08:30:31 +0000

    Init Airflow alone works

---
+++ docker-pgsql-load/ingest_data.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import argparse
+
+from time import time
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+    csv_name = '/tmp/output.csv'
+
+    import os
+
+    if os.path.exists(csv_name):
+        print(f"File '{csv_name}' already exists. Skipping download.")
+    else:
+        print(f"Downloading '{csv_name}'...")
+        os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    counter = 0
+    max_chunks = 10
+
+    while counter < max_chunks:
+        try:
+            t_start = time()
+
+            df = next(df_iter)
+
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+            print(f'Inserted chunk {counter + 1}, took {t_end - t_start:.3f} seconds')
+
+            counter += 1
+
+        except StopIteration:
+            print('Completed — no more data.')
+            break
+
+    print(f'Loop ended after {counter} chunk(s).')
+
+
+
+if __name__ == '__main__':
+    # Parse the command line arguments and calls the main program
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', help='user name for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the csv file')
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ docker-pgsql-load/pipeline.py
@@ -0,0 +1,8 @@
+import sys
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+print(f'Finished job successfully for the day f{day}')
